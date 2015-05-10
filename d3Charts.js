7da6039215ff0953816f979c30233cb0ba449884
sajidsan <sajid@jut.io>
2015-05-10 16:03:22 -0700

    updated with working x axis

--- d3Charts.js
+++ d3Charts.js
@@ -8,33 +8,113 @@ function d3Scope(data, tabletop) {
   //set myData equal to 'data' so that I can access it outside of thsi function
   myData2 = data;
   //i can't figure out how to move this d3 function outside of the scope of this other function. whatever
-  d3.select("#chart1")
-  .selectAll("div")
-    .data(myData2)
-  .enter().append("div")
-    .style("width", function(d) { return d.howwasyourday * 30 + "px"; })
-    //.style("height", 10)
-    .style("background-color", "blue")
-    .style("color", "white")
-    .text(function(d) { return d.timestamp; });
+  
+
+  
+
+  var w = 2000;
+  var h = 200;
+  var barPadding = 1;
+  var barWidth = w / (data.length) - barPadding;
+  var margin = {top: 10, right: 20, bottom: 20, left: 10};
+
+  var xScale = d3.time.scale()
+  	.domain([new Date(data[0].timestamp), new Date(data[data.length - 1].timestamp)])
+  	.range([0, w]);
+
+  console.log("the date of the start = " + xScale(new Date(data[data.length - 1].timestamp)));
+  console.log("a date " + new Date(data[data.length - 1].timestamp));
+
+  var yScale = d3.scale.linear()
+  	.domain([0, 10])
+  	.range([0, h - margin.top - margin.bottom]);
+
+  var xAxis = d3.svg.axis()
+  	.scale(xScale)
+  	.orient('bottom')
+  	.ticks(d3.time.months)
+  	.tickFormat(d3.time.format('%b, %d'))
+  	.tickSize(0)
+  	.tickPadding(8);
+
+  var svg = d3.select("#chart1")
+  	.append("svg")
+  	.attr("width", w)
+  	.attr("height", h);
+
+  svg.selectAll("rect")
+  	.data(data)
+  	.enter()
+  	.append("rect")
+  	.attr("x", function(d) {
+  		return xScale(new Date(d.timestamp));
+  	})
+  	.attr("y", function(d) {
+  		return h - yScale(d.howwasyourday);
+  	})
+  	.attr("width", 2)
+  	.attr("height", function(d) {
+  		return yScale(d.howwasyourday) - margin.bottom;
+  	})
+  	.attr("fill", "teal")
+  	.append("title")
+  	.text(function(d) {
+  		return d.didanythingfuninterestingormemorablehappen;
+  	})
+  	.on("mouseover", function(d) {
+  		var xPosition = parseFloat(d3.select(this).attr("x"));
+  		var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + h / 2;
+
+  		d3.select("#tooltip")
+						.style("left", xPosition + "px")
+						.style("top", yPosition + "px")						
+						.select("#value")
+						.text(d.timestamp);
+			   
+		//Show the tooltip
+		d3.select("#tooltip").classed("hidden", false);
+  	})
+  	.on("mouseout", function() {
+			   
+	//Hide the tooltip
+	d3.select("#tooltip").classed("hidden", true);
+					
+	})
+  	;
+
+  svg.selectAll("text")
+  	.data(data)
+  	.enter()
+  	.append("text")
+  	.text(function(d) {
+  		return d.howwasyourday;
+  	})
+  	.attr("x", function(d) {
+  		return xScale(new Date(d.timestamp));
+  	})
+  	.attr("y", function(d) {
+  		return h - yScale(d.howwasyourday) + 10;
+  	})
+  	.attr({
+  		"font-family": "sans-serif",
+  		"font-size": "11px",
+  		"fill": "black"
+  	})
+  	;
+
+  svg.append('g')
+  	.attr('class', 'x axis')
+  	.attr('transform', 'translate(0, ' + (h - margin.bottom) + ')')
+  	.call(xAxis);
+
+  	
 
 
 
 
 
-  var chart = d3.select("#chart1");
-  var div = chart.append("div");
-  div.html(myData2[0].timestamp);
 
   } // end of function 3
-
-
-
-
-
-
-var shortData = [42, 50, 8, 7];
-
 
 
 
